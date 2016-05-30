2b6dafd5f31dee98729624c0b970f99610b64280
Björn Kihlberg <hoppfull@gmail.com>
2016-05-30 13:58:59 +0200

    implemented cost function and fixed bug in squared error function

--- Gradient descent/src/GradientDescent.Tests.ts
+++ Gradient descent/src/GradientDescent.Tests.ts
@@ -1,6 +1,7 @@
 module GradientDescent.Tests {
     export function RunAll() {
         UnitTesting.Success('GradientDescent.DefHypothesis', testDefHypothesis)
+        UnitTesting.Success('GradientDescent.DefError', testDefError)
         UnitTesting.Success('GradientDescent.DefCost', testDefCost)
     }
 
@@ -27,20 +28,63 @@ module GradientDescent.Tests {
         ].every(test => test)
     }
 
+    function testDefError(msg: string) {
+        let msgGen = UnitTesting.GenMsgGen(msg, 1)
+        let assert = UnitTesting.GenAsserter(msgGen)
+        let errorTest = UnitTesting.GenErrorTester(msgGen)
+        return [
+            assert(DefSquaredError(DefHypothesis([1]))([], 10) === 81),
+            assert(DefSquaredError(DefHypothesis([2]))([], 33) === 961),
+            assert(DefSquaredError(DefHypothesis([5]))([], -2) === 49),
+            assert(DefSquaredError(DefHypothesis([19]))([], 7) === 144),
+
+            assert(DefSquaredError(DefHypothesis([1, 1]))([1], 7) === 25),
+            assert(DefSquaredError(DefHypothesis([5, -37]))([8], 8) === 89401),
+            assert(DefSquaredError(DefHypothesis([8, 10]))([10], 5) === 10609),
+            assert(DefSquaredError(DefHypothesis([-6, 10]))([9], 2) === 6724),
+
+            assert(DefSquaredError(DefHypothesis([9, -1, -8]))([1, 3], 13) === 841),
+            assert(DefSquaredError(DefHypothesis([9, -1, -8]))([6, -9], 37) === 1444),
+
+            errorTest(() => { DefSquaredError(DefHypothesis([]))([], 0) }),
+            errorTest(() => { DefSquaredError(DefHypothesis([]))([0], 0) }),
+            errorTest(() => { DefSquaredError(DefHypothesis([0]))([0], 0) }),
+            errorTest(() => { DefSquaredError(DefHypothesis([0]))([0, 0], 0) }),
+        ].every(test => test)
+    }
+
     function testDefCost(msg: string) {
         let msgGen = UnitTesting.GenMsgGen(msg, 1)
         let assert = UnitTesting.GenAsserter(msgGen)
         let errorTest = UnitTesting.GenErrorTester(msgGen)
         return [
-            assert(DefCost(DefHypothesis([1]))([], 10) === -9),
-            assert(DefCost(DefHypothesis([2]))([], 33) === -31),
-            assert(DefCost(DefHypothesis([5]))([], -2) === 7),
-            assert(DefCost(DefHypothesis([19]))([], 7) === 12),
+            assert(DefCost([
+                [0, 0, 0],
+                [1, 1, 0],
+                [3, 7, 9]
+            ], [0, 0, 0])([0, 0, 0, 0]) === 0),
+            assert(DefCost([
+                [1, 1],
+            ], [3])([1, 1, 1]) === 0),
+            assert(DefCost([
+                [1, 3],
+            ], [3])([1, 1, 2]) === 25),
+            assert(DefCost([
+                [1, 3],
+                [6, -9],
+            ], [13, 37])([9, -1, -8]) === 1142.5),
+            assert(DefCost([
+                [6, 8, 14],
+                [42, -78, 9],
+            ], [1, 52])([5, -8, 4, -7]) === 293332),
 
-            assert(DefCost(DefHypothesis([1, 1]))([1], 7) === -5),
-            assert(DefCost(DefHypothesis([5, -37]))([8], 8) === -299),
-            assert(DefCost(DefHypothesis([8, 10]))([10], 5) === 103),
-            assert(DefCost(DefHypothesis([-6, 10]))([9], 2) === 82),
+            errorTest(() => { DefCost([[]], [])([]) }),
+            errorTest(() => { DefCost([], [0])([]) }),
+            errorTest(() => { DefCost([[]], [0])([]) }),
+            errorTest(() => { DefCost([[0]], [])([]) }),
+            errorTest(() => { DefCost([[0]], [0])([]) }),
+            errorTest(() => { DefCost([[0, 0]], [0])([0, 0]) }),
+            errorTest(() => { DefCost([[0, 0], [0, 0],], [0])([0, 0]) }),
         ].every(test => test)
     }
 }
--- Gradient descent/src/GradientDescent.ts
+++ Gradient descent/src/GradientDescent.ts
@@ -3,7 +3,7 @@ module GradientDescent {
     type DefHypothesisSig = (ws: number[]) => HypothesisSig
     export let DefHypothesis: DefHypothesisSig = ws => {
         if (ws.length < 1)
-            throw Error("GradientDescent.Hypothesis: ws.length < 1")
+            throw Error("GradientDescent.DefHypothesis: ws.length < 1")
         return xs => {
             if (ws.length !== xs.length + 1)
                 throw Error("GradientDescent.Hypothesis: ws.length !== xs.length + 1")
@@ -11,7 +11,21 @@ module GradientDescent {
         }
     }
 
-    type CostSig = (xs: number[], y: number) => number
-    type DefCostSig = (h: HypothesisSig) => CostSig
-    export let DefCost: DefCostSig = h => (xs, y) => h(xs) - y
+    type SquaredErrorSig = (xs: number[], y: number) => number
+    type DefSquaredErrorSig = (h: HypothesisSig) => SquaredErrorSig
+    export let DefSquaredError: DefSquaredErrorSig = h => (xs, y) => (h(xs) - y)**2
+    
+    type CostSig = (ws: number[]) => number
+    type DefCostSig = (xss:number[][], ys:number[]) => CostSig
+    export let DefCost:DefCostSig = (xss, ys) => {
+        if (xss.length !== ys.length)
+            throw Error("GradientDescent.DefCost: xss.length !== ys.length")
+        return ws => {
+            let h = DefHypothesis(ws)
+            let error = DefSquaredError(h)
+            let sumSquared = xss.reduce((acc, xs, i) => acc + error(xs, ys[i]), 0)
+            let avg = sumSquared / ys.length
+            return avg
+        }
+    }
 }
