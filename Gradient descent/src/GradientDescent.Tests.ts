4f3a78821a1f86a22edc5e48c464289342d05985
Björn Kihlberg <hoppfull@gmail.com>
2016-06-04 00:19:19 +0200

    begun testing on error function

--- Gradient descent/src/GradientDescent.Tests.ts
+++ Gradient descent/src/GradientDescent.Tests.ts
@@ -2,8 +2,28 @@ module GradientDescent.Tests {
     export function RunAll() {
         UnitTesting.Success('GradientDescent.err', testErr)
     }
-    
-    let testErr: UnitTesting.UnitTestSig = (assert, errorTest) => [
-        true
-    ].every((test: boolean) => test)
+
+    let testErr: UnitTesting.UnitTestSig = (assert, errorTest) => {
+        let ts = [10, 20, 30]
+        let ys = [1, 2, 3]
+        let xss = [
+            [1, 1],
+            [2, 2],
+            [3, 3]
+        ]
+        console.log(ts.slice(1).reduce((a, t, j) => a + t * xss[0][j], ts[0] - ys[0]))
+
+        //console.log(err(xss, ys, ts))
+
+
+        return [
+            assert(err([
+                [1, 1],
+                [2, 2],
+                [3, 3]
+            ],
+                [1, 2, 3],
+                [10, 20, 30])[0] === 59)
+        ].every((test: boolean) => test)
+    }
 }
--- Gradient descent/src/GradientDescent.ts
+++ Gradient descent/src/GradientDescent.ts
@@ -1,5 +1,5 @@
 module GradientDescent {
     type errSig = (xss: number[][], ys: number[], ts: number[]) => number[]
-    let err: errSig = (xss, ys, ts) =>
-        xss.map((xs, i) => xs.reduce((acc, x, j) => acc + x*ts[j] - ys[i], 0))
+    export let err: errSig = (xss, ys, ts) =>
+        xss.map((xs, i) => ts.slice(1).reduce((a, t, j) => a + t * xs[j], ts[0] - ys[i]))
 }
